acb2d2e798ec97c6f9920b71828897ef5ad4bdef
Bharat Balaji D <bharatbalaji95@gmail.com>
2025-08-01 18:15:42 +0530

    main

---
+++ infra/vpc.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = "vpc-0769425c66212e1c7" # Replace with your actual VPC ID
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "web-sg"
+    Environment = "dev"
+  }
+}
